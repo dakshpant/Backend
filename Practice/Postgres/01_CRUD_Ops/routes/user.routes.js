c01b9f226478e22c9a5aa7c5dc42ed98e30a955e
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2025-10-09 15:36:11 +0530

    CRUD Operations Views usingAJAX and EJS

--- Practice/Postgres/01_CRUD_Ops/routes/user.routes.js
+++ Practice/Postgres/01_CRUD_Ops/routes/user.routes.js
@@ -3,9 +3,14 @@ import userController from "../controllers/user.controller.js"
  
 const route = express.Router();
 
+route.get("/getAllUsers", async(req, res)=>{
+    res.render("index");
+})
+
 route.get("/", userController.homeRoute);
 route.post("/createUser", userController.createUser);
-route.get("/getAllUsers", userController.getAllUsers);
+route.get("/getAllUsers/json", userController.getAllUsers)
+
 route.put("/updateUser/:id", userController.updateUser);
 route.delete("/deleteUser/:id", userController.deleteUser);
 
--- Practice/Postgres/01_CRUD_Ops/server.js
+++ Practice/Postgres/01_CRUD_Ops/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 // Connect Database
 await connectDB();
 
