809f07eff5510ab416bf7cb92432e1843c6ddbcb
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2025-10-10 10:30:24 +0530

    CRUD VIEWS DONE

--- Practice/Postgres/01_CRUD_Ops/routes/user.routes.js
+++ Practice/Postgres/01_CRUD_Ops/routes/user.routes.js
@@ -1,15 +1,23 @@
-import express from "express"
-import userController from "../controllers/user.controller.js"
- 
+import express from "express";
+import User from "../models/user.model.js";
+import userController from "../controllers/user.controller.js";
+
+
 const route = express.Router();
 
-route.get("/getAllUsers", async(req, res)=>{
-    res.render("index");
-})
+route.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.render("index", { title: "User List", users });
+    // res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 route.get("/", userController.homeRoute);
 route.post("/createUser", userController.createUser);
-route.get("/getAllUsers/json", userController.getAllUsers)
+route.get("/getAllUsers/json", userController.getAllUsers);
 
 route.put("/updateUser/:id", userController.updateUser);
 route.delete("/deleteUser/:id", userController.deleteUser);
